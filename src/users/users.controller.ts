44b2c909bccb4e28f766d7d646a7e32da430bd1c
YuryHouse <yury.house@yahoo.com>
2023-01-09 17:46:39 +0300

    feature: roles guard was added

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,24 +1,51 @@
-import { Body, Controller, Get, Post } from "@nestjs/common";
+import { Body, Controller, Get, Post, UseGuards } from "@nestjs/common";
 import { CreateUserDto } from "./dto/create-user.dto";
 import { UsersService } from "./users.service";
 import { ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
 import { Users } from "./users.model";
+import { Roles } from "../auth/roles-auth.decorator";
+import { RolesGuard } from "../auth/roles.guard";
+import { AddRoleDto } from "./dto/add-role.dto";
+import { BanUserDto } from "./dto/ban-user.dto";
 
-@ApiTags('Users')
-@Controller('users')
+@ApiTags("Users")
+@Controller("users")
 export class UsersController {
   constructor(private usersService: UsersService) {
   }
-  @ApiOperation({summary: 'User creation'})
-  @ApiResponse({status: 200, type: Users})
+
+  @ApiOperation({ summary: "User creation" })
+  @ApiResponse({ status: 200, type: Users })
   @Post()
   create(@Body() userDto: CreateUserDto) {
     return this.usersService.createUser(userDto);
   }
-  @ApiOperation({summary: 'Get all users'})
-  @ApiResponse({status: 200, type: [Users]})
+
+  @ApiOperation({ summary: "Get all users" })
+  @ApiResponse({ status: 200, type: [Users] })
+  @Roles("ADMIN")
+  @UseGuards(RolesGuard)
   @Get()
-  getAll(){
+  getAll() {
     return this.usersService.getAllUsers();
+  }
+
+
+  @ApiOperation({ summary: "Get role" })
+  @ApiResponse({ status: 200 })
+  @Roles("ADMIN")
+  @UseGuards(RolesGuard)
+  @Post("/role")
+  addRole(@Body() dto: AddRoleDto) {
+    return this.usersService.addRole(dto);
+  }
+
+  @ApiOperation({ summary: "Ban user" })
+  @ApiResponse({ status: 200 })
+  @Roles("ADMIN")
+  @UseGuards(RolesGuard)
+  @Post("/ban")
+  ban(@Body() dto: BanUserDto) {
+    return this.usersService.ban(dto);
   }
 }
