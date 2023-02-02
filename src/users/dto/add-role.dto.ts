72ad5fdd8af3754fbced0ec5d65ded8c4096cf7b
YuryHouse <yury.house@yahoo.com>
2023-02-02 21:08:16 +0300

    feature: Pipes were added

--- src/users/dto/add-role.dto.ts
+++ src/users/dto/add-role.dto.ts
@@ -1,6 +1,9 @@
+import { IsNumber, IsString } from "class-validator";
 
 
 export class AddRoleDto {
+  @IsString({message: "Have to be a string"})
   readonly value: string;
+  @IsNumber({}, {message: "Have to be a number"})
   readonly userId: number;
 }
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,8 +1,13 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { IsEmail, IsString, Length } from "class-validator";
 
 export class CreateUserDto {
-  @ApiProperty({ example: 'user@mail.com', description: 'email' })
+  @ApiProperty({ example: "user@mail.com", description: "email" })
+  @IsString({ message: "Have to be string" })
+  @IsEmail({}, { message: "Incorrect email" })
   readonly email: string;
-  @ApiProperty({ example: '12345678', description: 'password' })
+  @ApiProperty({ example: "12345678", description: "password" })
+  @IsString({ message: "Have to be string" })
+  @Length(4, 16, { message: "Your password length must be greater than 4 and less than 16" })
   readonly password: string;
 }
